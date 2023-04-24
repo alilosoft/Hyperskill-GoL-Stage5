1258398ee2a92ecbc41fa040d4a5afa394f77325
alilosoft <fellahi.ali@gmail.com>
2023-04-24 20:50:12 +0100

    custom font

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 package life;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
+        // Use a custom font for labels
+        File fontFile = new File("./resources/Nunito-SemiBold.ttf");
+        try {
+            Font nunitoFont = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+            UIManager.put("Label.font", nunitoFont.deriveFont(16f));
+        } catch (FontFormatException | IOException e) {
+            System.err.println("Can't create font: Nunito");
+            e.printStackTrace();
+        }
         SwingUtilities.invokeAndWait(GameOfLife::new);
     }
 }
